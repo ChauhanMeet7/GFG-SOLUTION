371fa58355b119a69bf6e8d4cf198f165171396b
Meet Chauhan <108236696+ChauhanMeet7@users.noreply.github.com>
2022-07-12 21:47:44 +0530

    Interview Question

---
+++ Array/pair with given sum in a sorted array.cpp
@@ -0,0 +1,24 @@
+class Solution{
+  public:
+    int Countpair(int arr[], int n, int sum){
+        
+        // Complete the function
+        int i=0,j=n-1,k=0,ans=0;
+        while(i<j){
+            k=arr[i]+arr[j];
+            if(k==sum){
+                ans++;
+                i++;
+                j--;
+                
+            }else if(k<sum){
+                j--;
+            }else{
+                i++;
+            }
+        }
+    if(ans==0) return -1;
+    return ans;
+        
+    }
+};
